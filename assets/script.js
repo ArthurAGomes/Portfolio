544768beaea118ec3d8c5f2f2f9ba0174e3abc0d
Arthur <153244771+ArthurAGomes@users.noreply.github.com>
2024-05-25 02:45:59 +0000

    estrutura e estilo completos

--- assets/script.js
+++ assets/script.js
@@ -17,11 +17,11 @@ function saudacao() {
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Toggle dark mode
     const toggleSwitch = document.getElementById('toggle-switch');
     const body = document.body;
-    
+
     toggleSwitch.addEventListener('click', () => {
         body.classList.toggle('dark');
     });
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Toggle navigation menu
     const menuToggle = document.getElementById('menu-toggle');
     const navMenu = document.getElementById('myNavMenu');
-    
+
     menuToggle.addEventListener('click', () => {
         navMenu.classList.toggle('responsive');
     });
@@ -49,3 +49,75 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+// Scroll animation 
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".text-info", { delay: 200 });
+sr.reveal(".text-btn", { delay: 200 });
+sr.reveal(".socical_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 320 });
+
+sr.reveal(".project-box", { interval: 200 });
+sr.reveal(".top-header", {})
+
+const srLeft = ScrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+})
+
+srLeft.reveal(".about-info", { delay: 100 });
+srLeft.reveal(".contact-info", { delay: 100 });
+
+const srRight = ScrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+})
+
+srRight.reveal(".skill", { delay: 100 });
+srRight.reveal(".skill-box", { delay: 100 });
+
+// Menu active 
+const sections = document.querySelectorAll(".section[id]");
+
+function scrollActive() {
+    const scrolly = window.scrollY;
+
+    let foundActive = false; 
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+
+        const link = document.querySelector(".nav-menu a[href*='" + sectionId + "']");
+
+        if (scrolly > sectionTop && scrolly <= sectionTop + sectionHeight) {
+            link.classList.add("active-link");
+            foundActive = true; // Set the flag when an active section is found
+        } else {
+            link.classList.remove("active-link");
+        }
+    });
+
+
+    const homeLink = document.querySelector(".nav-menu a[href*='home']");
+    if (!foundActive) {
+        homeLink.classList.add("active-link");
+    } else {
+        homeLink.classList.remove("active-link");
+    }
+}
+
+window.addEventListener("scroll", scrollActive);
+
